4e7bcd7ee33250ec072ce4b2db20582fb4d303cf
yulieh <36465599+yulieh@users.noreply.github.com>
2022-12-01 17:45:50 +0100

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -2,26 +2,61 @@ import os
 import json
 from matplotlib import pyplot as plt
 
-ls_files = os.listdir('data')
-print(ls_files)
+ls_files = os.listdir("data")
 
 average_time_ls = []
 correct_rate_ls = []
-for ls in ls_files:
-    with open('data/'+ls,'r') as f :
+average_entropy_ls = []
+
+time_ls = []
+entropy_ls = []
+responses_ls = []
+
+for file in ls_files:
+    with open("data/" + file, "r") as f:
         data = json.load(f)
-        average_time_ls.append(data['Average Time'])
-        correct_rate_ls.append(data['Correct Rate'])
-print(average_time_ls)
-print(correct_rate_ls)
-y=average_time_ls
-x=[i for i in range(len(y))]
-plt.scatter(x,y)
-plt.scatter(x,correct_rate_ls )
+        average_time_ls.append(data["Average Time"])
+        correct_rate_ls.append(data["Correct Rate"])
+        average_entropy_ls.append(data["Average Entropy"])
+
+        time_ls += data["Resonse Time"]
+        entropy_ls += data["Entropy Score"]
+        responses_ls += data["Responses"]
+
+
+plt.scatter([i for i in range(len(average_time_ls))], average_time_ls)
+plt.scatter([i for i in range(len(correct_rate_ls))], correct_rate_ls)
+plt.scatter([i for i in range(len(average_entropy_ls))], average_entropy_ls)
+plt.legend(["Average Time", "Correct Rate", "Average Entropy"])
+plt.xlabel("Recording #")
+plt.ylabel("Result")
+plt.title("Average scores of each recording")
 plt.show()
 
-#complexity vs correct rate
 
-#complexity vs average time
+plt.scatter(entropy_ls, time_ls)
+plt.xlabel("Entropy")
+plt.ylabel("Response time")
+plt.title("Response time vs Entropy")
+plt.show()
+
+score = [0 for i in range(int(round(max(entropy_ls))) + 1)]
+count = [0 for i in range(int(round(max(entropy_ls))) + 1)]
+
+for i in range(len(responses_ls)):
+    level = int(round(entropy_ls[i]))
+    score[level] += responses_ls[i]
+    count[level] += 1
+
+for i in range(len(score)):
+    try:
+        score[i] /= count[i]
+    except ZeroDivisionError:
+        score[i] = 0
 
 
+plt.plot([i for i in range(len(score))], score)
+plt.xlabel("Correct Rate")
+plt.ylabel("Entropy Level")
+plt.title("Correct rate per entropy level")
+plt.show()
--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import random
 import os
 import json
 from datetime import datetime
+
+import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.backend_bases import MouseButton
+from skimage.filters.rank import entropy
+from skimage.color import rgb2gray
+from skimage.morphology import disk
 
 click_cord_x = -1
 click_cord_y = -1
@@ -12,65 +17,81 @@ click_cord_y = -1
 
 def on_click(event):
     if event.button is MouseButton.LEFT:
-        print(f'data coords (x={event.xdata} y={event.ydata})')
         global click_cord_x, click_cord_y
         click_cord_x = event.xdata
         click_cord_y = event.ydata
-        plt.close('all')
+        plt.close("all")
 
-ls_files=os.listdir('pictures')
-print(ls_files)
+
+ls_files = os.listdir("pictures")
+if not len(ls_files):
+    raise Exception("No images found in pictures folder")
 
 time_ls = []
 correct_ls = []
-for codchen in range(10):
+entropy_ls = []
+
+print("Please click on location of the picture on the right in the picture on the left")
+
+for _ in range(10):
     img_name = random.choice(ls_files)
     img = plt.imread("pictures/" + img_name)
-    #x=img[:,:,0]
-    #print(len(x[1]))
-    # histogram=plt.hist(x)
-    # print(len(histogram[2]))
-    # print(len([i for i in histogram[0] if i>0.]))
-    square_size = int(min(img.shape[1], img.shape[0])/10)
+    square_size = int(min(img.shape[1], img.shape[0]) / 5)
     x1 = random.randint(0, img.shape[1] - square_size)
     x2 = x1 + square_size
     y1 = random.randint(0, img.shape[0] - square_size)
     y2 = y1 + square_size
     img2 = img[y1:y2, x1:x2, :]
 
+    plt.subplots(1, 2, gridspec_kw={"width_ratios": [3, 1]}, figsize=(12, 8))
+
     plt.subplot(1, 2, 1)
+    plt.title("Original Image")
     plt.imshow(img)
 
     plt.subplot(1, 2, 2)
+    plt.title("Piece to Find")
     plt.imshow(img2)
 
     t0 = time.time()
-    plt.connect('button_press_event', on_click)
+    plt.connect("button_press_event", on_click)
+    plt.suptitle("Images")
+
     plt.show()
+
     t1 = time.time()
     spent_time = t1 - t0
-    print(spent_time)
     time_ls.append(spent_time)
 
     if x1 < click_cord_x < x2 and y1 < click_cord_y < y2:
-        print("Correct!")
         correct_ls.append(1)
     else:
-        print("Naaah")
         correct_ls.append(0)
 
-print(sum(time_ls)/len(time_ls))
-print(sum(correct_ls)/len(correct_ls))
+    img2_gray = rgb2gray(img2)
+    entropy_image = entropy(img2_gray, disk(round(square_size / 50)))
+    entropy_ls.append(np.mean(entropy_image))
 
-data_dict={'Resonse Time:': time_ls,
-           'Average Time': sum(time_ls)/len(time_ls),
-           'Responses':correct_ls,
-           'Correct Rate':sum(correct_ls)/len(correct_ls)}
-print("Hello Reinis, your average time is {} a"
-      "nd your correct rate is {}. "
-      "You should be better than this!".format(data_dict['Average Time'], data_dict['Correct Rate']))
+data_dict = {
+    "Resonse Time": time_ls,
+    "Average Time": sum(time_ls) / len(time_ls),
+    "Responses": correct_ls,
+    "Correct Rate": sum(correct_ls) / len(correct_ls),
+    "Entropy Score": entropy_ls,
+    "Average Entropy": sum(entropy_ls) / len(entropy_ls),
+}
+print(
+    "Hello, your average time is {} a"
+    "nd your correct rate is {}. Average entropy is {}".format(
+        data_dict["Average Time"],
+        data_dict["Correct Rate"],
+        data_dict["Average Entropy"],
+    )
+)
 
-now=datetime.now()
-date_time=now.strftime("%d-%m-%Y-%H-%M")
-with open('data/'+date_time+".json",'w') as f:
-    json.dump(data_dict,f)
+now = datetime.now()
+date_time = now.strftime("%d-%m-%Y-%H-%M")
+with open("data/" + date_time + ".json", "w") as f:
+    json.dump(data_dict, f)
+
+print("Results saved to {} file".format(date_time + ".json"))
